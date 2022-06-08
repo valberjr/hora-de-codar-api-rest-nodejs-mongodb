e3eae930d270979bd6ad79eb26befcaf45d04564
valberjr <3cifhzht@duck.com>
2022-06-08 10:24:53 -0300

    atualizando dados

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -56,4 +56,26 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+// UPDATE - atualização de dados (PUT, PATCH)
+router.patch('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    const { name, salary, approved } = req.body;
+
+    const person = { name, salary, approved };
+
+    try {
+        const updatedPerson = await Person.updateOne({ _id: id }, person);
+
+        if (updatedPerson.matchedCount === 0) {
+            res.status(404).json({ error: 'Pessoa não encontrada' });
+            return;
+        }
+
+        res.status(200).json(person);
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+});
+
 module.exports = router;

f7579d33438ddc252fac778c540ef7e0cfee835e
valberjr <3cifhzht@duck.com>
2022-06-08 10:06:39 -0300

    listando dados cadastrados

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const Person = require('../models/Person');
 
+// Create - criação de dados
 router.post('/', async (req, res) => {
     // req.body
     const { name, salary, approved } = req.body;
@@ -20,6 +21,17 @@ router.post('/', async (req, res) => {
         res.status(201).json({
             message: 'Pessoa inserida no sistema com sucesso!',
         });
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+});
+
+// READ - leitura de dados
+router.get('/', async (req, res) => {
+    try {
+        const people = await Person.find();
+
+        res.status(200).json(people);
     } catch (error) {
         res.status(500).json({ error: error });
     }

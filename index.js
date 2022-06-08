a3f10fc9c0e3415350b60715983a7bc9e2cac630
valberjr <3cifhzht@duck.com>
2022-06-08 09:16:16 -0300

    configuração do projeto e primeiro endpoint

---
+++ index.js
@@ -0,0 +1,17 @@
+// config inicial
+const express = require('express');
+const app = express();
+
+// forma de ler JSON / middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// rota inicial /endpoint
+app.get('/', (req, res) => {
+    // mostrar req
+
+    res.json({ message: 'Oi Express' });
+});
+
+// entregar uma porta
+app.listen(3000);

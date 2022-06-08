751d5037b2b7fad9acb31fc9ad2bdf0429f88bf3
valberjr <3cifhzht@duck.com>
2022-06-08 09:56:25 -0300

    inserindo dados

--- index.js
+++ index.js
@@ -10,6 +10,30 @@ const Person = require('./models/Person');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// rotas da API
+app.post('/person', async (req, res) => {
+    // req.body
+    const { name, salary, approved } = req.body;
+
+    const person = { name, salary, approved };
+
+    if (!name) {
+        res.status(422).json({ error: 'O nome é obrigatório' });
+    }
+
+    // create
+    try {
+        // criando dados
+        await Person.create(person);
+
+        res.status(201).json({
+            message: 'Pessoa inserida no sistema com sucesso!',
+        });
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+});
+
 // rota inicial /endpoint
 app.get('/', (req, res) => {
     // mostrar req

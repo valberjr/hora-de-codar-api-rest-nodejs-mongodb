30d7242dd2df982fc9314e450ab696b827698fd6
valberjr <3cifhzht@duck.com>
2022-06-08 10:32:00 -0300

    removendo cadastro

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -78,4 +78,24 @@ router.patch('/:id', async (req, res) => {
     }
 });
 
+// DELETE - remoção de dados
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    const person = await Person.findOne({ _id: id });
+
+    if (!person) {
+        res.status(422).json({ error: 'Pessoa não encontrada' });
+        return;
+    }
+
+    try {
+        await Person.deleteOne({ _id: id });
+
+        res.status(200).json({ message: 'Pessoa removida com sucesso!' });
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+});
+
 module.exports = router;
